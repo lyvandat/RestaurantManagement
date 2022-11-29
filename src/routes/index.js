cd2f78d8c7857ae7f1459dd38cc9ec07fa63fdff
Phuc Dat <bluehairmylove2014@gmail.com>
2022-11-30 01:46:02 +0700

    full list page and full responsive'

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const productsRouter = require('./products');
 const cartRouter = require('./cart');
 const itemRouter = require('./item');
 const buyRouter = require('./buy');
+const listRouter = require('./list');
 
 function route(app) {
+    app.use('/list', listRouter);
     app.use('/buy', buyRouter);
     app.use('/item', itemRouter);
     app.use('/cart', cartRouter);
