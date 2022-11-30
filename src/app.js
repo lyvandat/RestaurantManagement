1d7ecfbfd4a256d99e9ced2f0160ddef977d52cb
lyvandat <75932518+lyvandat@users.noreply.github.com>
2022-11-30 21:09:13 +0700

    prepare deploy 3

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+const methodOverride = require("method-override");
+const { engine } = require("express-handlebars");
+const app = express();
+const route = require("./routes");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use(methodOverride("_method"));
+
+// Custom middlewares
+// app.use(SortMiddleware);
+
+// HTTP logger
+// app.use(morgan('combined'));
+
+// Template engines
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+      isFirstVis: (vr) => {
+        if (vr == 0) return true;
+        else return false;
+      },
+      loops: (n, block) => {
+        var accum = "";
+        for (var i = 0; i < n; ++i) accum += block.fn(i);
+        return accum;
+      },
+    },
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources", "views"));
+
+// Routes init
+route(app);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,63 +1,13 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const methodOverride = require('method-override');
-const { engine } = require('express-handlebars');
-
+const app = require("./app");
 // const SortMiddleware = require('./app/middlewares/SortMiddleware');
 
-const app = express();
 const port = 3000;
 
-const route = require('./routes');
-const db = require('./config/db');
+const db = require("./config/db");
 
 // Connect to DB
 db.connect();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
-app.use(express.json());
-
-app.use(methodOverride('_method'));
-
-// Custom middlewares
-// app.use(SortMiddleware);
-
-// HTTP logger
-// app.use(morgan('combined'));
-
-// Template engines
-app.engine(
-    'hbs',
-    engine({
-        extname: '.hbs',
-        helpers: {
-            sum: (a, b) => a + b,
-            isFirstVis: (vr) => {
-                if(vr == 0) return true; 
-                else return false;
-            },
-            loops: (n, block) => {
-                var accum = '';
-                for(var i = 0; i < n; ++i)
-                    accum += block.fn(i);
-                return accum;
-            },
-        },
-    }),
-);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources', 'views'));
-
-// Routes init
-route(app);
-
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
