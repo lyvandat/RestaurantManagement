42327c34e4da5f2895463784cf0de0884b9c7c98
lyvandat <75932518+lyvandat@users.noreply.github.com>
2022-12-09 11:35:14 +0700

    refactor CartSchema, add addItem method

--- src/app/models/Cart.js
+++ src/app/models/Cart.js
@@ -1,17 +1,54 @@
 const mongoose = require('mongoose');
-const ProductQuantity = require('./ProductQuantitySchema');
-
 const Schema = mongoose.Schema;
 
 const CartSchema = new Schema(
     {
-        userID: { type: String, required: true },
-        products: { type: [ProductQuantity] },
+        userID: { 
+            type: Schema.Types.ObjectId, required: true,
+            ref: "User"
+         },
+         products: [{
+            productId: Number,
+            quantitiy: Number,
+            name: String,
+            manufacturer: String,
+            price: Number,
+         }],
+         active: {
+            type: Boolean,
+            default: true
+         },
+         modifiedOn: {
+            type: Date,
+            default: Date.now()
+         }
     },
     {
         timestamps: true,
     },
 );
+
+CartSchema.methods.addItemToCart = async function({productId, quantity, name, manufacturer, price}) {
+    try {
+        let productItemIndex = this.products.find((prod) => prod.productId === productId);
+        let newUpdatedItem = null;
+        // if item exists
+        if (productItemIndex !== -1) {
+            newUpdatedItem = this.products[productItemIndex];
+            newUpdatedItem.quantity += quantity;
+            this.products[productItemIndex] = newUpdatedItem;
+        } 
+        // if item does not exist
+        else {
+            newUpdatedItem = {productId, quantity, name, manufacturer, price}
+            this.products.push(newUpdatedItem);
+        }
+
+        await this.save();
+    } catch(err) {
+        console.log(err.message);
+    }
+}
 
 // Add plugin
 
